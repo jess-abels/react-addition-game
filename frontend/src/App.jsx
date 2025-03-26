021b42f9a7b9279db4757d0586459dc36fef276d
Jessica Abels <jessabels92@gmail.com>
2025-03-26 16:19:29 -0700

    Fix another merge conflict

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
 import LandingPage from './pages/LandingPage';
 import './App.css'
 import {Routes,Route} from 'react-router-dom'; 
 import AdditionGame from './pages/AdditionGame';
-=======
-import LandingPage from './components/LandingPage';
-import './App.css'
->>>>>>> 5a9b975f03e4ed8c12509532071f0e8f2728dd7a
 
 function App() {
 
