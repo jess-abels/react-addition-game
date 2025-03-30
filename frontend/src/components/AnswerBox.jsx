3a0377c9ed519a61bc57daf4d33433eb91dce198
Jessica Abels <jessabels92@gmail.com>
2025-03-30 16:18:06 -0700

    Make answer options unique

--- frontend/src/components/AnswerBox.jsx
+++ frontend/src/components/AnswerBox.jsx
@@ -1,23 +1,16 @@
 import './AnswerBox.css'
-import { getRandomIntInclusive, getRandomNumsEqualLessThanOrEqualToTen } from '../utils/RandomNumGenerator'
+import { getThreeUniqueAnswerOptions } from '../utils/RandomNumGenerator'
 
 export default function AnswerBox(props){
  let {correctAnswer} = props;
- let randomNum1 = getRandomIntInclusive(1, 10);
- let randomNum2 = getRandomIntInclusive(1, 10);
+ let answerOptions = getThreeUniqueAnswerOptions(correctAnswer);
 
     return (
         <div>
             <h2>How many cats are there altogether?</h2>
-        <div>
-           <button>{randomNum1}</button>
-       </div>
-           <div>
-           <button> {randomNum2}</button>
-       </div>
-           <div>
-           <button>{correctAnswer}</button>
-       </div>
+            {answerOptions.map((answerOption) => (
+        <button key={answerOption}>{answerOption}</button>
+      ))}
        </div>
     )
 }
--- frontend/src/utils/RandomNumGenerator.jsx
+++ frontend/src/utils/RandomNumGenerator.jsx
@@ -10,3 +10,13 @@ export function getRandomIntInclusive(min, max) {
     const secondNumber = getRandomIntInclusive(1, 10 - firstNumber)
     return [firstNumber, secondNumber];
   }
+
+  export function getThreeUniqueAnswerOptions(correctAnswer){
+    let answerOptions = [correctAnswer];
+    do {
+        answerOptions.push(getRandomIntInclusive(1,10));
+        answerOptions = [...new Set(answerOptions)]
+    } while (answerOptions.length < 3);
+    return answerOptions;
+  }
+
