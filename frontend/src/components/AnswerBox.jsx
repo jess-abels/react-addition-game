c8dacd47e9cb5f4e1e4e04a07213076b758e70dd
Jessica Abels <jessabels92@gmail.com>
2025-04-06 14:52:44 -0700

    Check value of button clickedfor answer

--- frontend/src/components/AnswerBox.jsx
+++ frontend/src/components/AnswerBox.jsx
@@ -5,11 +5,18 @@ export default function AnswerBox(props){
  let {correctAnswer} = props;
  let answerOptions = getThreeUniqueAnswerOptions(correctAnswer);
 
+ function checkAnswer(event) {
+    let answer = event.target.value;
+    if (answer === correctAnswer.toString()) {
+        console.log("Good job!")
+    }
+ }
+
     return (
         <div>
             <h2>How many cats are there altogether?</h2>
             {answerOptions.map((answerOption) => (
-        <button key={answerOption}>{answerOption}</button>
+        <button value={answerOption} onClick={checkAnswer} key={answerOption}>{answerOption}</button>
       ))}
        </div>
     )
